2f002e3908581f19d02ff6e1522ae782d9296acb
Roman Developer <romzes13@gmail.com>
2017-01-08 23:26:26 -0500

    New service implemented.
    Signed-off-by: Roman Developer <romzes13@gmail.com>

---
+++ web-handyman/src/com/web/handyman/service/HandymanService.java
@@ -0,0 +1,14 @@
+package com.web.handyman.service;
+
+import java.util.List;
+
+import com.web.handyman.entity.Handyman;
+import com.web.handyman.entity.User;
+
+public interface HandymanService {
+
+	public List<User> getUsers();
+
+	
+	
+}
---
+++ web-handyman/src/com/web/handyman/service/HandymanServiceImpl.java
@@ -0,0 +1,30 @@
+package com.web.handyman.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.web.handyman.dao.UserDAO;
+import com.web.handyman.entity.Handyman;
+import com.web.handyman.entity.User;
+
+@Service
+public class HandymanServiceImpl implements HandymanService {
+
+		// need to inject user DAO
+		@Autowired
+		private UserDAO userDAO;
+	
+	
+		@Override
+		@Transactional
+		public List<User> getUsers() {
+			
+			
+	
+			return userDAO.getUsers();
+		}
+
+}
