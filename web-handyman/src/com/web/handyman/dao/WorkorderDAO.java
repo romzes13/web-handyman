917be66643afd417c79a47019c76de3fd71f0366
Roman Developer <romzes13@gmail.com>
2017-01-12 18:24:56 -0500

    New Workorder DAO class added.
    Signed-off-by: Roman Developer <romzes13@gmail.com>

---
+++ web-handyman/src/com/web/handyman/dao/WorkorderDAO.java
@@ -0,0 +1,17 @@
+package com.web.handyman.dao;
+
+import java.util.List;
+
+import com.web.handyman.entity.WorkOrder;
+
+public interface WorkorderDAO {
+
+	List<WorkOrder> getWorkOrders();
+
+	void saveWorkOrder(WorkOrder theWorkOrder);
+
+	WorkOrder getWorkOrder(int theId);
+
+	void deleteWorkorder(int theId);
+
+}
